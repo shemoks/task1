4f889907dfba2a8d2175a21b8fcabcd7ae3728e1
shemoks <shemshur@ukr.net>
2015-10-17 17:55:12 +0300

    Создание функции нахождния простых чисел в заданном диапазоне

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html >
+<link rel="stylesheet" type="text/css" href="style.css">
+<script></script>
+<head>
+    <meta charset="UTF-8">
+    <title>HomeWork 2</title>
+</head>
+<body>
+<form action="numbers.php" method="post">
+    <input type="text" name="firstNumber" />
+    <input type="text" name="secondNumber" />
+    <input type="submit" value="go">
+</form>
+</body>
+</html>
---
+++ numbers.php
@@ -0,0 +1,42 @@
+<?php
+$firstNumber = $_POST['firstNumber'];
+$secondNumber = $_POST['secondNumber'];
+
+function simpleNumbers($firstNumber, $secondNumber)
+{
+    $key = 0;
+    for ($i = $firstNumber; $i <= $secondNumber; $i++) {
+        $count = 0;
+        for ($j = 1; $j <= $i; $j++)
+            if ($i % $j == 0) {
+                $count++;
+            }
+        if ($count == 2 && $i != 1) {
+            $node[$key] = $i;
+            $key++;
+        }
+    }
+    return $node;
+}
+
+
+function viewTable($node)
+{
+    $table = "<table>";
+    $table .= "<tr>";
+    foreach ($node as $val) {
+        $table .= "<td>";
+        $table .= $val;
+        $table .= "</td>";
+
+    }
+    $table .= "</tr>";
+    $table .= "</table>";
+
+    return $table;
+}
+
+echo "Простые числа </br>";
+$numbers = simpleNumbers($firstNumber, $secondNumber);
+echo viewTable($numbers);
+?>
